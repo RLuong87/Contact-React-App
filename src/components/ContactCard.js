ec3d7390903ce66d73f0ccf5e1b969f5c6a6a942
Richies Luong <richiesluong.student@careerdevs.com>
2021-12-29 23:06:04 -0500

    Adding images and learning methods to add contacts

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ContactList from "./ContactList";
+import user from "../images/user.png";
 
 const Contactcard = (props) => {
     const {id, name, email} = props.contact;
   return (
     <div className="item">
+        <img className="ui avatar image" src={user} alt="user" />
       <div className="content">
         <div className="header">{name}</div>
         <div>{email}</div>
