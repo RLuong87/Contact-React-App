cfab1a6b100f3addb921603778a80a6b1b580d14
Richies Luong <richiesluong.student@careerdevs.com>
2022-01-01 19:09:43 -0500

    Updated AddContact and ContactCard classes

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -6,18 +6,37 @@ class AddContact extends React.Component {
         email: "",
     }
 
+        add = (e) => {
+            e.preventDefault();
+            if (this.state.name === "" && this.state.email === "") {
+                alert("All the fields are mandatory!");
+                return;
+            }
+            console.log(this.state);
+        }
     render() {
         return(
             <div className="ui main">
                 <h2>AddContact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name" value={this.state.name} onChange={ (e) => this.setState({ name: e.target.value }) }/>
+                        <input 
+                        type="text" 
+                        name="name" placeholder="Name" 
+                        value={this.state.name} 
+                        onChange={ (e) => this.setState({ name: e.target.value }) }
+                        />
                     </div>
                     <div className="field">
                         <label>Email</label>
-                        <input type="text" name="email" placeholder="Email" />
+                        <input 
+                        type="text" 
+                        name="email" 
+                        placeholder="Email"
+                        value={this.state.email} 
+                        onChange={ (e) => this.setState({ email: e.target.value }) } 
+                        />
                     </div>
                     <button className="ui button blue">Add</button>
                 </form>
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,10 +2,10 @@ import React from "react";
 import user from "../images/user.png";
 
 const Contactcard = (props) => {
-    const {id, name, email} = props.contact;
+  const { id, name, email } = props.contact;
   return (
     <div className="item">
-        <img className="ui avatar image" src={user} alt="user" />
+      <img className="ui avatar image" src={user} alt="user" />
       <div className="content">
         <div className="header">{name}</div>
         <div>{email}</div>
